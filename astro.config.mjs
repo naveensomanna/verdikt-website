d44b49f6bc7dbbd579952d84982d747a6f857c79
naveenSuperleap <naveensomanna26@gmail.com>
2026-03-05 13:23:03 +0530

    fix: updated domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.verdikt.com', // Change to your production URL
+  site: 'https://www.verdikt.in',
   integrations: [sitemap()],
 });
